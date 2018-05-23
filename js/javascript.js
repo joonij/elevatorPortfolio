118a72388e13643560ac6c25350a5458a80fdb2a
joonij <37970610+joonij@users.noreply.github.com>
2018-05-23 17:20:01 +0900

    release 2018/05/23

--- js/javascript.js
+++ js/javascript.js
@@ -1,98 +1,21 @@
 $(document).ready(function(){
-    var ht = $(window).height()
+    var ht =$(window).height();
     
     $("#container").css("height",ht);
     
-    if($("#floor_4").hasClass("on")) {
-        $("#button_3").click(function(){
-            $(".floor").removeClass("on");
-            $("#elevator").animate({top:"200px"},3000,"easeInOutQuad");
-            $("#floor_3").addClass("on");
-        });
-        $("#button_2").click(function(){
-            $(".floor").removeClass("on");
-            $("#elevator").animate({top:"400px"},3000,"easeInOutQuad");
-            $("#floor_2").addClass("on");
-        });
-        $("#button_1").click(function(){
-            $(".floor").removeClass("on");
-            $("#elevator").animate({top:"600px"},3000,"easeInOutQuad");
-            $("#floor_1").addClass("on");
-        });
-    }
-    else if($("#floor_3").hasClass("on")) {
-        $("#button_4").click(function(){
-            $(".floor").removeClass("on");
-            $("#elevator").animate({top:"200px"},3000,"easeInOutQuad");
-            $("#floor_4").addClass("on");
-        });
-        $("#button_2").click(function(){
-            $(".floor").removeClass("on");
-            $("#elevator").animate({top:"400px"},3000,"easeInOutQuad");
-            $("#floor_2").addClass("on");
-        });
-        $("#button_1").click(function(){
-            $(".floor").removeClass("on");
-            $("#elevator").animate({top:"600px"},3000,"easeInOutQuad");
-            $("#floor_1").addClass("on");
-        });
-    }
-    if($("#floor_2").hasClass("on")) {
-        $("#button_4").click(function(){
-            $(".floor").removeClass("on");
-            $("#elevator").animate({top:"200px"},3000,"easeInOutQuad");
-            $("#floor_4").addClass("on");
-        });
-        $("#button_3").click(function(){
-            $(".floor").removeClass("on");
-            $("#elevator").animate({top:"400px"},3000,"easeInOutQuad");
-            $("#floor_3").addClass("on");
-        });
-        $("#button_1").click(function(){
-            $(".floor").removeClass("on");
-            $("#elevator").animate({top:"600px"},3000,"easeInOutQuad");
-            $("#floor_1").addClass("on");
-        });
-    }
-    if($("#floor_1").hasClass("on")) {
-        $("#button_4").click(function(){
-            $(".floor").removeClass("on");
-            $("#elevator").animate({top:"200px"},3000,"easeInOutQuad");
-            $("#floor_4").addClass("on");
-        });
-        $("#button_3").click(function(){
-            $(".floor").removeClass("on");
-            $("#elevator").animate({top:"400px"},3000,"easeInOutQuad");
-            $("#floor_3").addClass("on");
-        });
-        $("#button_2").click(function(){
-            $(".floor").removeClass("on");
-            $("#elevator").animate({top:"600px"},3000,"easeInOutQuad");
-            $("#floor_2").addClass("on");
-        });
-    }
-    
-    
-    
-//    $("#button_4").click(function(){
-//        $(".floor").removeClass("on");
-//        $("#elevator").animate({top:"0px"},3000,"easeInOutQuad");
-//        $("#floor_4").addClass("on");
-//    });
-//    $("#button_3").click(function(){
-//        $(".floor").removeClass("on");
-//        $("#elevator").animate({top:"200px"},3000,"easeInOutQuad");
-//        $("#floor_3").addClass("on");
-//    });
-//    $("#button_2").click(function(){
-//        $(".floor").removeClass("on");
-//        $("#elevator").animate({top:"400px"},3000,"easeInOutQuad");
-//        $("#floor_2").addClass("on");
-//    });
-//    $("#button_1").click(function(){
-//        $(".floor").removeClass("on");
-//        $("#elevator").animate({top:"600px"},3000,"easeInOutQuad");
-//        $("#floor_1").addClass("on");
-//    });
-    
+    $("#button_3").click(function(){
+        $(".floor").removeClass("on");
+        $("#elevator").animate({top:"0px"},3000,"easeInOutQuad");
+        $("#floor_3").addClass("on");
+    });
+    $("#button_2").click(function(){
+        $(".floor").removeClass("on");
+        $("#elevator").animate({top:"250px"},3000,"easeInOutQuad");
+        $("#floor_2").addClass("on");
+    });
+    $("#button_1").click(function(){
+        $(".floor").removeClass("on");
+        $("#elevator").animate({top:"500px"},3000,"easeInOutQuad");
+        $("#floor_1").addClass("on");
+    });
 });
