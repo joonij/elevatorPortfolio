b5b6abd29862916bcd28ef1746c49399b6d37af1
joonij <37970610+joonij@users.noreply.github.com>
2018-05-25 17:00:43 +0900

    release 2018/05/25

--- js/javascript.js
+++ js/javascript.js
@@ -3,27 +3,64 @@ $(document).ready(function(){
     
     $("#container").css("height",ht);
     
+    $("#button_area").hide();
     
     $(".elevator_sw").click(Event, function(){
-        $("#left_door").animate({right:"300px"},4000);
-        $("#right_door").animate({right:"300px"},3000);
-        $("#button_area"
+        $(".left_door").animate({right:"130px"},3000);
+        $(".right_door").animate({left:"-65px"},3000);
+        $("#button_area").show(3000);
     });
     
     
-    $("#button_3").click(function(){
-        $(".floor").removeClass("on");
-        $("#move_ele").animate({top:"50px"},3000,"easeInOutQuad");
-        $("#floor_3").addClass("on");
+    $("#button_3").click(Event, function(){
+        $("#button_area").hide(3000);
+        $(".left_door").stop().animate({right:"65px"},3000);
+        $(".right_door").stop().animate({left:"65px"},3000, function(){
+            $(".floor_door, .floor").removeClass("on", function(){
+                $("#button_area").stop().animate({top:"50px"},3000, function(){
+                    $("#door_3, #floor_3").addClass("on", function(){
+                        $(".left_door").delay(3000).stop().animate({right:"130px"},3000);
+                        $(".right_door").delay(3000).stop().animate({left:"-65px"},3000, function(){
+                            $(".left_door").stop().animate({right:"65px"},3000);
+                            $(".right_door").stop().animate({left:"65px"},3000);
+                        });
+                    });
+                });
+            });
+        });
     });
-    $("#button_2").click(function(){
-        $(".floor").removeClass("on");
-        $("#move_ele").animate({top:"300px"},3000,"easeInOutQuad");
-        $("#floor_2").addClass("on");
+    $("#button_2").click(Event, function(){
+        $("#button_area").hide(3000);
+        $(".left_door").stop().animate({right:"65px"},3000);
+        $(".right_door").stop().animate({left:"65px"},3000, function(){
+            $(".floor_door, .floor").removeClass("on", function(){
+                $("#button_area").stop().animate({top:"300px"},3000, function(){
+                    $("#door_2, #floor_2").addClass("on", function(){
+                        $(".left_door").delay(3000).stop().animate({right:"130px"},3000);
+                        $(".right_door").delay(3000).stop().animate({left:"-65px"},3000, function(){
+                            $(".left_door").stop().animate({right:"65px"},3000);
+                            $(".right_door").stop().animate({left:"65px"},3000);
+                        });
+                    });
+                });
+            });
+        });
     });
-    $("#button_1").click(function(){
-        $(".floor").removeClass("on");
-        $("#move_ele").animate({top:"550px"},3000,"easeInOutQuad");
-        $("#floor_1").addClass("on");
+    $("#button_1").click(Event, function(){
+        $("#button_area").hide(3000);
+        $(".left_door").stop().animate({right:"65px"},3000);
+        $(".right_door").stop().animate({left:"65px"},3000, function(){
+            $(".floor_door, .floor").removeClass("on", function(){
+                $("#button_area").stop().animate({top:"550px"},3000, function(){
+                    $("#door_1, #floor_1").addClass("on", function(){
+                        $(".left_door").delay(3000).stop().animate({right:"130px"},3000);
+                        $(".right_door").delay(3000).stop().animate({left:"-65px"},3000, function(){
+                            $(".left_door").stop().animate({right:"65px"},3000);
+                            $(".right_door").stop().animate({left:"65px"},3000);
+                        });
+                    });
+                });
+            });
+        });
     });
 });
