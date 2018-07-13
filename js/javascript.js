58dd57677cbbb1b4a562430e43205853c5b37b2f
joonij <37970610+joonij@users.noreply.github.com>
2018-07-13 17:28:48 +0900

    release 20180713

--- js/javascript.js
+++ js/javascript.js
@@ -46,33 +46,32 @@ $(document).ready(function(){
     $(".elevator_sw").click(Event, function(){
         $(".left_door").stop().animate({right:"130px"},2000);
         $(".right_door").stop().animate({left:"-65px"},2000);
-//        $("#button_3").css("background", "url(img/button_off_3.png) no-repeat");
+        $("#button_3").css("background", "url(img/button_off_3.png) no-repeat");
         $("#button_2").css("background", "url(img/button_off_2.png) no-repeat");
         $("#button_1").css("background", "url(img/button_off_1.png) no-repeat");
         $("#button_area").delay(2000).show(1000);
         $(".floor > i").delay(2000).fadeOut(1000);
     });
     
-//    $("#button_3").click(Event, function(){
-//        $(this).css("background", "url(img/button_on_3.png) no-repeat");
-//        $("#button_area").hide(1000);
-//        $(".left_door").stop().animate({right:"65px"},2000);
-//        $(".right_door").stop().animate({left:"65px"},2000, function(){
-//            $(".floor_door, .floor").removeClass("on", function(){
-//                $("#button_area").stop().animate({top:"50px"},1500, function(){
-//                    $("#floor_door_3, #floor_3").addClass("on", function(){
-//                        $(".left_door").delay(3000).stop().animate({right:"130px"},2000);
-//                        $(".right_door").delay(3000).stop().animate({left:"-65px"},2000, function(){
-//                            $(".left_door").stop().animate({right:"65px"},2000);
-//                            $(".right_door").stop().animate({left:"65px"},2000);
-//                            $(".floor i").delay(2000).fadeIn(1000);
-//                            $(".project_area").not($("#floor_3 .project_area")).css("display","none");
-//                        });
-//                    });
-//                });
-//            });
-//        });
-//    });
+   $("#button_3").click(Event, function(){
+       $("#button_3").css("background", "url(img/button_on_3.png) no-repeat");
+       $("#button_area").hide(1000);
+       $(".left_door").stop().animate({right:"65px"},2000);
+       $(".right_door").stop().animate({left:"65px"},2000, function(){
+           $(".floor_door, .floor").removeClass("on", function(){
+               $("#button_area").stop().animate({top:"50px"},1500, function(){
+                   $("#floor_door_3, #floor_3").addClass("on", function(){
+                       $(".left_door").delay(3000).stop().animate({right:"130px"},2000);
+                       $(".right_door").delay(3000).stop().animate({left:"-65px"},2000, function(){
+                           $(".left_door").stop().animate({right:"65px"},2000);
+                           $(".right_door").stop().animate({left:"65px"},2000);
+                           $(".floor i").delay(2000).fadeIn(1000);
+                       });
+                   });
+               });
+           });
+       });
+   });
     $("#button_2").click(Event, function(){
         $("#button_2").css("background", "url(img/button_on_2.png) no-repeat");
         $("#button_area").hide(1000);
@@ -86,7 +85,6 @@ $(document).ready(function(){
                             $(".left_door").stop().animate({right:"65px"},2000);
                             $(".right_door").stop().animate({left:"65px"},2000);
                             $(".floor i").delay(2000).fadeIn(1000);
-                            $(".project_area").not($("#floor_2 .project_area")).css("display","none");
                         });
                     });
                 });
@@ -106,7 +104,6 @@ $(document).ready(function(){
                             $(".left_door").stop().animate({right:"65px"},2000);
                             $(".right_door").stop().animate({left:"65px"},2000);
                             $(".floor i").delay(2000).fadeIn(1000);
-                            $(".project_area").not($("#floor_1 .project_area")).css("display","none");
                         });
                     });
                 });
